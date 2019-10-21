0b33df7de16332a39a9b75fb5c31c404e304a467
daSchee <damian.scheerer@autentek.de>
2019-10-21 09:20:23 +0200

    PKW Liste Add

---
+++ git/PkwListe/PkwListe/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace PkwListe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variante mit Array:
+            // ==================
+            Pkw[] pkwArray = new Pkw[10];
+            pkwArray[0] = new Pkw("Fiat", "Panda", "rot");
+            pkwArray[1] = new Pkw("Opel", "Zafira", "grau");
+            int roteAutos = 0;
+            for (int i = 0; i < pkwArray.Length; i++)
+            {
+                Console.WriteLine("Auto " + i + ": ");
+                if (pkwArray[i] != null)
+                {
+                    Console.WriteLine(pkwArray[i].GetFarbe());
+                    if (pkwArray[i].GetFarbe().Equals("rot"))
+                    {
+                        roteAutos++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("nicht vorhanden");
+                }
+            }
+            Console.WriteLine(roteAutos + " rote Autos gefunden");
+
+            // Variante mit verketteter Liste:
+            // ==============================
+            Console.WriteLine("===============================================");
+            PkwListe pkwListe = new PkwListe();
+            pkwListe.FügePkwEin(new Pkw("Fiat", "Panda", "rot"));
+            pkwListe.FügePkwEin(new Pkw("Opel", "Zafira", "grau"));
+            pkwListe.FügePkwEin(new Pkw("Toyota", "Yaris", "grün"));
+            pkwListe.FügePkwEin(new Pkw("Mercedes", "A-Klasse", "gelb"));
+            pkwListe.FügePkwEin(new Pkw("Kia", "Seed", "silber"));
+            Console.Write(pkwListe.Select());
+            Console.WriteLine("Es gibt " + pkwListe.AnzahlPkws + " Pkws");
+
+            Console.ReadKey();
+        }
+    }
+
+    class PkwListe
+    {
+        private Pkw ersterPkw;
+        private Pkw letzterPkw;
+        private int anzahlPkws;
+
+        public PkwListe()
+        {
+            ersterPkw = letzterPkw = null;
+            anzahlPkws = 0;
+        }
+        public int AnzahlPkws
+        {
+            get { return anzahlPkws; }
+        }
+
+        public Pkw FügePkwEin(Pkw neuerPkw)
+        {
+            neuerPkw.SetNext(null);
+            if (ersterPkw == null)
+            {
+                ersterPkw = letzterPkw = neuerPkw;
+            }
+            else
+            {
+                letzterPkw.SetNext(neuerPkw);
+                letzterPkw = neuerPkw;
+            }
+            anzahlPkws++;
+            return neuerPkw;
+        }
+
+        public string Select()
+        {
+            Pkw tmp = ersterPkw;
+            string selectString = "";
+            while(tmp != null)
+            {
+                selectString += tmp.GetMarke() + " " + tmp.GetModell() + " " + tmp.GetFarbe() + "\n";
+                tmp = tmp.GetNext();
+            }
+            return selectString;
+        }
+    }
+    class Pkw
+    {
+        private String marke;
+        private String modell;
+        private String farbe;
+        private double leistung;
+        private double km;
+        private double geschwindigkeit;
+
+        // Für die Verkettung der Pkw-Objekte als Liste:
+        private Pkw nächsterPkw;
+
+        public Pkw(String marke, String modell, String farbe)
+        {
+            this.marke = marke;
+            this.modell = modell;
+            this.farbe = farbe;
+            this.geschwindigkeit = 0.0;
+            this.nächsterPkw = null;
+        }
+        public void SetLeistung(double kW)
+        {
+            leistung = kW;
+        }
+        public double FahreStrecke(double strecke)
+        {
+            km += strecke;
+            return km;
+        }
+        public String GetMarke()
+        {
+            return marke;
+        }
+        public String GetModell()
+        {
+            return modell;
+        }
+        public String GetFarbe()
+        {
+            return farbe;
+        }
+        public double GetKmStand()
+        {
+            return km;
+        }
+
+        internal Pkw GetNext()
+        {
+            return nächsterPkw;
+        }
+
+        internal void SetNext(Pkw neuerPkw)
+        {
+            nächsterPkw = neuerPkw;
+        }
+    }
+}
