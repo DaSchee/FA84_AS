cb1f1f4b823a09bc9a8e2f96a799038d8bb8e065
daSchee <damian.scheerer@autentek.de>
2020-01-27 08:58:00 +0100

    lolololol

---
+++ git/PersonErgaenzen/PersonErgaenzen/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace PersonErgänzen
+{
+    public class Person
+    {
+        private string name;
+        private DateTime birthday;
+                
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+        public Person() { }
+
+        public Person(string name, DateTime birthday)
+        {
+            this.name = name;
+            this.birthday = birthday;
+        }
+
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+        }
+
+        public DateTime Birthday
+        {
+            set { this.birthday = value; }
+            get { return this.birthday; }
+        }
+
+        public bool IsAtleast(int years)
+        {
+            int age = this.GetAge();
+            return age >= years;
+        }
+
+        public int GetAge()
+        {
+            int age;
+            age = DateTime.Now.Year - birthday.Year;
+            if (DateTime.Now.DayOfYear < birthday.DayOfYear)
+                age = age - 1;
+
+            return age;
+        }
+    }
+}
---
+++ git/PersonErgaenzen/PersonErgaenzen/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PersonErgänzen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person seventeen = new Person("Hugo", new DateTime(2001, 8, 30));
+            Person eighteen = new Person("Hugo", new DateTime(2001, 8, 29));
+            Console.WriteLine(seventeen.IsAtleast(18));
+            Console.WriteLine(eighteen.IsAtleast(18));
+            Console.WriteLine(seventeen.GetAge());
+            Console.WriteLine(eighteen.GetAge());
+        }
+    }
+}
